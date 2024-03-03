f7202959e5260b6c62e8536ecab81af99dc74822
bartexpl11 <175180@student.uwm.edu.pl>
2024-03-03 13:08:24 +0100

    zmiany

--- main.py
+++ main.py
@@ -22,8 +22,22 @@ print(f)
 f = a // b
 print(f)
 # g = a ** 2
-g = a ** 2
+g = a ** 2 # dwa ** to potega
 print(g)
+h = pow(a, 2)
+print(h)
+i = 6 ** 1/2  # kolejnosc dzialan , trzeba dac 1/2 w nawias
+j = pow(6, 1/2)
+print(i)
+print(j)
+
+k = 'wizualizacja danych'
+l = ' grupa '
+m = 1
+print(k+l+str(m))
+print('liczba a jest rowna {:d}, liczba b jest rowna {:.2f}'.format(a, b))
+# w klamerki wchodza zmienne z format, liczba zmiennych i klamer ma sie rownac
+
 
 
 
