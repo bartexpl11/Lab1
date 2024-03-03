df0b98d45b6643fecdc0cd4870f4a657aa2f37be
bartexpl11 <175180@student.uwm.edu.pl>
2024-03-03 12:52:51 +0100

    zmiany

--- main.py
+++ main.py
@@ -8,3 +8,22 @@ print(b)
 print(type(b))
 zmienna_tesktowa = 'wizualizacja danych'
 print(zmienna_tesktowa)
+print(type(zmienna_tesktowa))
+a = 6
+b = 3
+
+c = a + b
+print(c)
+d = a - b
+print(d)
+e = 4
+f = b // a
+print(f)
+f = a // b
+print(f)
+# g = a ** 2
+g = a ** 2
+print(g)
+
+
+
